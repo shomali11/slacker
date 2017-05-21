cb34d8d036e555d5f03fc437f6c619e29361b72a
Raed Shomali <shomali11@gmail.com>
2017-05-21 00:59:33 -0400

    Cleaned up format

--- expression/expression_test.go
+++ expression/expression_test.go
@@ -1,8 +1,8 @@
 package expression
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestMatch(t *testing.T) {
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,8 +1,8 @@
 package parser
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestBooleanParam(t *testing.T) {
