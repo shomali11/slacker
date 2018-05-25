3d655b831881476278f2a49c7d4b4ef162f77630
Javier Palomo Almena <javier.palomo@cabify.com>
2018-05-26 00:15:44 +0200

    Make BotCommand an interface. Have Slacker expose the list of commands.
    By making BotCommand an interface, we make it easier to mock test it.
    Exposing the available commands register in Slacker, allows us to write
    our own custom help function having access to the usage and description.

--- command.go
+++ command.go
@@ -6,30 +6,50 @@ import (
 )
 
 // NewBotCommand creates a new bot command object
-func NewBotCommand(usage string, description string, handler func(request Request, response ResponseWriter)) *BotCommand {
+func NewBotCommand(usage string, description string, handler func(request Request, response ResponseWriter)) BotCommand {
 	command := commander.NewCommand(usage)
-	return &BotCommand{usage: usage, description: description, handler: handler, command: command}
+	return &botCommand{usage: usage, description: description, handler: handler, command: command}
 }
 
-// BotCommand structure contains the bot's command, description and handler
-type BotCommand struct {
+// botCommand structure contains the bot's command, description and handler
+type botCommand struct {
 	usage       string
 	description string
 	handler     func(request Request, response ResponseWriter)
 	command     *commander.Command
 }
 
+// BotCommand interface
+type BotCommand interface {
+	Usage() string
+	Description() string
+
+	Match(text string) (*proper.Properties, bool)
+	Tokenize() []*commander.Token
+	Execute(request Request, response ResponseWriter)
+}
+
+// Usage returns the command usage
+func (c *botCommand) Usage() string {
+	return c.usage
+}
+
+// Description returns the command description
+func (c *botCommand) Description() string {
+	return c.description
+}
+
 // Match determines whether the bot should respond based on the text received
-func (c *BotCommand) Match(text string) (*proper.Properties, bool) {
+func (c *botCommand) Match(text string) (*proper.Properties, bool) {
 	return c.command.Match(text)
 }
 
 // Tokenize returns the command format's tokens
-func (c *BotCommand) Tokenize() []*commander.Token {
+func (c *botCommand) Tokenize() []*commander.Token {
 	return c.command.Tokenize()
 }
 
 // Execute executes the handler logic
-func (c *BotCommand) Execute(request Request, response ResponseWriter) {
+func (c *botCommand) Execute(request Request, response ResponseWriter) {
 	c.handler(request, response)
 }
