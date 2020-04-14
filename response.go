f8bec78e5c8f5a55e669402a0253e25ee809aa44
batagliao <lucas.batagliao@luizalabs.com>
2020-04-14 10:07:51 -0300

    enablin reply in thread

--- response.go
+++ response.go
@@ -13,31 +13,47 @@ const (
 // A ResponseWriter interface is used to respond to an event
 type ResponseWriter interface {
 	Reply(text string, options ...ReplyOption)
+	ReplyInThread(text string, options ...ReplyOption)
 	ReportError(err error)
+	ReportErrorInThread(err error)
 	Typing()
+	TypingInThread()
 	RTM() *slack.RTM
 	Client() *slack.Client
 }
 
 // NewResponse creates a new response structure
-func NewResponse(channel string, client *slack.Client, rtm *slack.RTM) ResponseWriter {
-	return &response{channel: channel, client: client, rtm: rtm}
+func NewResponse(event *slack.MessageEvent, client *slack.Client, rtm *slack.RTM) ResponseWriter {
+	return &response{event: event, client: client, rtm: rtm}
 }
 
 type response struct {
-	channel string
-	client  *slack.Client
-	rtm     *slack.RTM
+	event  *slack.MessageEvent
+	client *slack.Client
+	rtm    *slack.RTM
 }
 
 // ReportError sends back a formatted error message to the channel where we received the event from
 func (r *response) ReportError(err error) {
-	r.rtm.SendMessage(r.rtm.NewOutgoingMessage(fmt.Sprintf(errorFormat, err.Error()), r.channel))
+	r.rtm.SendMessage(r.rtm.NewOutgoingMessage(fmt.Sprintf(errorFormat, err.Error()), r.event.Channel))
+}
+
+// ReportErrorInThread sends back a formatted error message to the channel where we received the event from inside a thread to the previous message
+func (r *response) ReportErrorInThread(err error) {
+	r.rtm.SendMessage(r.rtm.NewOutgoingMessage(fmt.Sprintf(errorFormat, err.Error()), r.event.Channel, slack.RTMsgOptionTS(r.event.EventTimestamp)))
+
 }
 
 // Typing send a typing indicator
 func (r *response) Typing() {
-	r.rtm.SendMessage(r.rtm.NewTypingMessage(r.channel))
+	r.rtm.SendMessage(r.rtm.NewTypingMessage(r.event.Channel))
+}
+
+// TypingInThread send a typing indicator in a thread
+func (r *response) TypingInThread() {
+	message := r.rtm.NewTypingMessage(r.event.Channel)
+	message.ThreadTimestamp = r.event.EventTimestamp
+	r.rtm.SendMessage(message)
 }
 
 // Reply send a attachments to the current channel with a message
@@ -45,12 +61,27 @@ func (r *response) Reply(message string, options ...ReplyOption) {
 	defaults := newReplyDefaults(options...)
 
 	r.rtm.PostMessage(
-		r.channel,
+		r.event.Channel,
 		slack.MsgOptionText(message, false),
 		slack.MsgOptionUser(r.rtm.GetInfo().User.ID),
 		slack.MsgOptionAsUser(true),
 		slack.MsgOptionAttachments(defaults.Attachments...),
 		slack.MsgOptionBlocks(defaults.Blocks...),
+	)
+}
+
+// ReplyInThread send a attachments to the current channel with a message in a thread to the previous message
+func (r *response) ReplyInThread(message string, options ...ReplyOption) {
+	defaults := newReplyDefaults(options...)
+
+	r.rtm.PostMessage(
+		r.event.Channel,
+		slack.MsgOptionText(message, false),
+		slack.MsgOptionUser(r.rtm.GetInfo().User.ID),
+		slack.MsgOptionAsUser(true),
+		slack.MsgOptionAttachments(defaults.Attachments...),
+		slack.MsgOptionBlocks(defaults.Blocks...),
+		slack.MsgOptionTS(r.event.EventTimestamp),
 	)
 }
 
