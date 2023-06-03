09a956faeb4942bdf1811b1097eb645bc502bea0
Raed Shomali <shomali11@gmail.com>
2023-06-03 01:42:07 -0400

    chore: Clean up

--- examples/job-middleware/main.go
+++ examples/job-middleware/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"context"
 	"log"
-
-	//"os"
+	"os"
 
 	"github.com/shomali11/slacker/v2"
 )
@@ -12,8 +11,7 @@ import (
 // Showcase the ability to define Cron Jobs with middleware
 
 func main() {
-	bot := slacker.NewClient("xoxb-13360094916-2243791173942-G5ns4blGnmRL2EVdFvmwdn2r", "xapp-1-A027JMM1RV2-5374333520369-a8e99a2f622940d7cdc410f5b7129da2896e644eeb9a66d0e449ba19af3b00ff")
-	//bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 	bot.AddCommand("ping", &slacker.CommandDefinition{
 		Handler: func(ctx slacker.CommandContext) {
 			ctx.Response().Reply("pong")
